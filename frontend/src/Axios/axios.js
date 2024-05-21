b27c8ed991395ce6495d0fbea663c5cc91580cf2
Alson Fred E. Leyva <126301804+alsonfredleyva@users.noreply.github.com>
2024-05-21 19:04:40 +0800

    Update axios.js

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"https://todos-tath.onrender.com/api"
+    baseURL:"https://todos-production-e6fd.up.railway.app/api"
 })
 export default instance
