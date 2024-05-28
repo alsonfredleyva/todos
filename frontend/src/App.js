530813de306b777a6865d7c62fc704333baa0dc4
Alson Fred E. Leyva <126301804+alsonfredleyva@users.noreply.github.com>
2024-05-29 01:33:57 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import Register from './components/Register';
 import ForgotPassword from './components/forgotPassword/ForgotPassword';
 import ResetPassword from './components/forgotPassword/ResetPassword';
 import axios from './Axios/axios.js';
+import ChatBot from 'react-simple-chatbot';
 function App() {
   const token = JSON.parse(localStorage.getItem("authToken"));
   const [tasks, dispatch] = useReducer(taskReducer, [])
@@ -93,6 +94,78 @@ function App() {
               <Route path="/resetPassword" element={<ResetPassword />} />
             </Route>
           </Routes>
+          <ChatBot
+            steps={[
+              {
+                id: '1',
+                message: 'What is your name?',
+                trigger: '2',
+              },
+              {
+                id: '2',
+                user: true,
+                trigger: '3',
+              },
+              {
+                id: '3',
+                message: 'Hi {previousValue}, nice to meet you!',
+                trigger: '4',
+              },
+              {
+                id: '4',
+                message: 'Do you need help or Question?',
+                trigger: '5',
+              },
+              {
+                id: '5',
+                options: [
+                  { value: 1, label: 'Number 1, What is the purpose of this todo app?', trigger: '6' },
+                  { value: 2, label: 'Number 2, How Can I Contact the admins here?', trigger: '7' },
+                  { value: 3, label: 'Number 3, Can I add todos anytime?', trigger: '8' },
+                ],
+              },
+              {
+                id: '6',
+                message: 'The purpose of this Todo app is to save time for the user if they forgot what task they want todo they just come here in check the task',
+                trigger: '5',
+              },
+              {
+                id: '7',
+                options: [
+                  { value: 1, label: 'Admin 1, https://www.facebook.com/alsonfred.leyva.1', trigger: '9' },
+                  { value: 2, label: 'Admin 2, https://www.facebook.com/chinito.property', trigger: '10' },
+                  { value: 3, label: 'Admin 3, https://www.facebook.com/johnmark.tingcay', trigger: '11' },
+                  { value: 4, label: 'Admin 4, https://www.facebook.com/Arjeeyorong22', trigger: '12' },
+                ],
+              },
+              {
+                id: '8',
+                message: 'Yes, you can add up todos anytime anywhere',
+                trigger: 5,
+              },
+              {
+                id: '9',
+                message: 'fb/alsonfred.leyva.1',
+                trigger: 5,
+              },
+              {
+                id: '10',
+                message: 'fb/chinito.property',
+                trigger: 5,
+              },
+              {
+                id: '11',
+                message: 'fb/johnmark.tingcay',
+                trigger: 5,
+              },
+              {
+                id: '12',
+                message: 'fb/Arjeeyorong22',
+                trigger: 5,
+              },
+            ]}
+            floating={true}
+          />
         </TaskContext.Provider>
       </TokenContext.Provider>
     </BrowserRouter>
